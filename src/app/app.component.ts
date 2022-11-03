fa124c5d2b726b90cde264bf2e7c089bb84161d8
SSteinmayer <samira.steinmayer@adesso.de>
2022-11-03 09:26:59 +0100

    simple start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  password = '';
+
  onButtonClick() {
-   console.log('Button was clicked');
- }
+this.password = 'MY PASSWORD!'}
+
+
+  getPassword() {
+   return this.password;
+  }
+
 }
