ecb2e58862a53c2f1f8787a9d86384bf1bb2c633
SSteinmayer <samira.steinmayer@adesso.de>
2022-11-03 11:09:55 +0100

    checking for password neccessities, bug in checking length

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {parse} from '@angular/compiler-cli/linker/babel/src/babel_core';
+import * as _babelNamespace from '@babel/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,10 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  length = 0;
   includeLetters = false;
   includeNumbers = false;
   includeSymbols = false;
   password = '';
+
+  //input is string output should be number
+  onChangeLength(value: string) {
+    //parsedValue is the actual input
+  const parsedValue = parseInt(value);
+
+  if (!isNaN(parsedValue)) {
+    this.length = parsedValue;
+  }
+};
 
   //OnChangeHandlers for the checkboxes:
   onChangeUseLetters() {
@@ -25,11 +39,36 @@ export class AppComponent {
   };
 
  onButtonClick() {
-   console.log(this.includeLetters);
-   console.log(this.includeNumbers);
-   console.log(this.includeSymbols);
-this.password = 'MY PASSWORD!'}
+   const numbers = '1234567890';
+   const letters = 'qwertzuiopasdfghjklyxcvbnm';
+   const symbols = '!%&/()=?:;';
 
+   console.log("Includes Letters: ", this.includeLetters);
+   console.log("Includes Numbers: ", this.includeNumbers);
+   console.log("Includes symbols: ", this.includeSymbols);
+
+   let validChars='';
+   if (this.includeLetters) {
+     validChars += letters;
+   }
+
+   if (this.includeNumbers) {
+     validChars += numbers;
+   }
+
+   if (this.includeSymbols) {
+     validChars += symbols;
+   }
+
+   let generatedPassword = '';
+   for (let i = 0; i<this.length; i++){
+     const index = Math.floor(Math.random()* validChars.length);
+    generatedPassword += validChars[index];
+   }
+
+//this.password = 'MY PASSWORD!'
+this.password = generatedPassword;
+ }
 
   getPassword() {
    return this.password;
